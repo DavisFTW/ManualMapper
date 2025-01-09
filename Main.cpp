2073668c3d28618121c553a97435f891584de485
unknown <davisrafaelsdainis@gmail.com>
2025-01-09 18:10:59 +0200

    Improved reading, LoadDLLFile now takes care of loading dll file and allocating memory for it

--- Main.cpp
+++ Main.cpp
@@ -16,24 +16,35 @@ int main() {
     const DWORD procId = Utils::getProcId(procName);
     if (procId == Utils::PROCESS_NOT_FOUND) {
         std::cerr << "Process not found: " << procName << "\n";
+        system("PAUSE");
         return EXIT_FAILURE;
     }
     std::cout << "Found process ID: " << procId << "\n";
 
-    // Resolve DLL
-    const auto dllSize = Utils::resolveDll(dllPath);
-    if (!dllSize) {
-        std::cerr << "Failed to resolve DLL at: " << dllPath << "\n";
-        return EXIT_FAILURE;
-    }
-    std::cout << "DLL size: " << *dllSize << " bytes\n";
 
     // Elevate privilege
     if (!Utils::ElevatePrivilege(SE_DEBUG_NAME)) {
         std::cerr << "Failed to elevate SE_DEBUG_NAME privilege.\n";
+        system("PAUSE");
         return EXIT_FAILURE;
     }
     std::cout << "Privilege elevated successfully.\n";
+
+    HANDLE procHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, procId);
+    if (!procHandle) {
+        DWORD Err = GetLastError();
+        std::cerr << "Failed to Open Process ERR: " << Err << std::endl;
+        system("PAUSE");
+        return EXIT_FAILURE;
+    }
+
+    BYTE* pSrcData = nullptr; // Will hold the DLL data
+    SIZE_T fileSize = 0;      // Will hold the size of the DLL file
+
+    if (!Utils::LoadDllFile(dllPath, pSrcData, fileSize, procHandle)) {
+        printf("Failed to load DLL file.\n");
+        return EXIT_FAILURE;
+    }
 
     return EXIT_SUCCESS;
 }
